4c91a086c3367e0d753899a15d0b463359d187a9
Bo Vandersteene <bo.vandersteene@reibo.be>
2018-10-15 15:54:32 +0200

    Update server.ts

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
-const port = 4000;
+const port = 2000;
 
 http
   .createServer((request, res) => {
@@ -16,23 +16,30 @@ http
 
     try {
       if (queryString) {
+        // Create a request including the queryString
         output = fs.readFileSync(`./server${pathname}/${request.method}_${queryString}.json`, 'utf8');
       } else {
         output = fs.readFileSync(`./ server${pathname} /${request.method}.json`, 'utf8');
       }
+      responseCode = output.statusCode || 200;
       if (output.error) {
-        responseCode = 400;
+        // If a file contains an error, the statuscode of that file will be taken or by default 400
+        responseCode = output.statusCode || 400;
         output = JSON.stringify({ error: output.error });
       }
     } catch (exception) {
+      // If a file is not found, by default 404
       responseCode = 404;
       output = JSON.stringify({ error: exception });
     }
 
+    // Allows you to use CORS
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Method', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', '*');
+
+    // Write the right resonsecode to your response head
     res.writeHead(responseCode, { 'Content-type': 'application/json' });
     res.end(output);
   })
